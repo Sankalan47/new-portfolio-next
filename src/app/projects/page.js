527638b8d721a562d8e331fd996b6a1903193a2f
Sankalan47 <sankalandasgupta@gmail.com>
2025-04-07 15:32:18 +0530

    Modified project and experience section

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -5,6 +5,13 @@ import Project from "@/components/Project/Project";
 const Projects = () => {
   const projects = [
     {
+      name: "BookOnABudget",
+      url: "https://explore.bookonabudget.com",
+      img: "https://ik.imagekit.io/sankalan/portfolio/projects/Screenshot%20(97).png?updatedAt=1699642487178",
+      desc: "End to end travel booking platoform for budget travelers. Built with Next.js, Firebase, and Tailwind CSS.",
+      tags: ["Next", "Firebase", "Mongodb", "Node.js", "Express", "Razorpay", "Resend", "Tailwind"],
+    },
+    {
       name: "Devfest Siliguri 2023",
       url: "https://gdgsiliguri.com/",
       img: "https://ik.imagekit.io/sankalan/portfolio/projects/Screenshot%20(97).png?updatedAt=1699642487178",
